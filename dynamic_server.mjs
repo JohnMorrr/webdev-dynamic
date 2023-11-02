92babe99e1770eaba1115fa934d7d3ab6194bd6c
John <101375121+JohnMorrr@users.noreply.github.com>
2023-11-02 16:13:06 -0500

    Update dynamic_server.mjs

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -38,15 +38,16 @@ app.use(express.static(root));
 
 
 //John Morrison's Dynamic Route
+//John Morrison's Dynamic Route
 app.get('/major_category/:category', (req, res) => { 
     let cat  = req.params.category;
     if(cat=="Biology"){
-      cat = "Biology & Life Science";
+      cat = "Biology & Life Science";  
     }else if(cat=="CompSci"){
       cat = "Computers & Mathematics";
     }
 
-  
+    //promises
     let prom1 = dbSelect('SELECT * FROM recent_grads WHERE  major_category = ?', [cat]);
     let prom2 = dbSelect('SELECT * FROM all_ages WHERE  major_category = ?', [cat]);
     let prom3 = fs.promises.readFile(path.join(template, 'major_category.html'), 'utf-8');
@@ -68,9 +69,38 @@ app.get('/major_category/:category', (req, res) => {
         alert(`404 Error: Major Category Not Found: '${cat}' `);
       }
       response = response.replace('$$TABLE_BODY$$', table_body);
-  
+
+      //Text generation
+      if(cat == "Biology & Life Science"){
+        response = response.replace('No information available', 'Biology is the scientific study of life. It is a natural science with a broad scope but has several unifying themes that tie it together as a single, coherent field. For instance, all organisms are made up of cells that process hereditary information encoded in genes, which can be transmitted to future generations. Another major theme is evolution, which explains the unity and diversity of life. Energy processing is also important to life as it allows organisms to move, grow, and reproduce. Finally, all organisms are able to regulate their own internal environments. *via wikipedia' );
+        response=response.replace('$$image.png$$', 'https://t.ly/tyRPo');
+      }else if(cat =="Computers & Mathematics"){
+        response=response.replace('No information available', 'Computer science is the study of computation, information, and automation. Computer science spans theoretical disciplines (such as algorithms, theory of computation, and information theory) to applied disciplines (including the design and implementation of hardware and software). Though more often considered an academic discipline, computer science is closely related to computer programming. *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/gG_Xy');
+      }else if(cat=="Arts"){
+        response = response.replace('No information available', 'Arts in education is an expanding field of educational research and practice informed by investigations into learning through arts experiences. In this context, the arts can include Performing arts education (dance, drama, music), literature and poetry, storytelling, Visual arts education in film, craft, design, digital arts, media and photography. *via wikipedia ');
+        response=response.replace('$$image.png$$', 'https://t.ly/1g4M9');
+      }else if(cat=="Engineering"){
+        response = response.replace('No information available', 'Engineering is the practice of using natural science, mathematics, and the engineering design process to solve technical problems, increase efficiency and productivity, and improve systems. Modern engineering comprises many subfields which include designing and improving infrastructure, machinery, vehicles, electronics, materials, and energy systems. *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/wFzoc');
+      }else if(cat=="Health"){
+        response = response.replace('No information available', 'Health sciences – those sciences that focus on health, or health care, as core parts of their subject matter. Health sciences relate to multiple academic disciplines, including STEM disciplines and emerging patient safety disciplines (such as social care research). *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/jYN84');
+      }else if(cat=="Business"){
+        response = response.replace('No information available', 'Business studies, often simply called business, is a field of study that deals with the principles of business, management, and economics. It combines elements of accountancy, finance, marketing, organizational studies, human resource management, and operations. Business studies is a broad subject,[2] where the range of topics is designed to give the student a general overview of the various elements of running a business. The teaching of business studies is known as business education *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/tpMYw');
+      }else if(cat=="Physical Sciences"){
+        response = response.replace('No information available','Physical science is a branch of natural science that studies non-living systems, in contrast to life science. It in turn has many branches, each referred to as a "physical science", together is called the "physical sciences" history of the branch of natural science that studies non-living systems, in contrast to the life sciences. It in turn has many branches, each referred to as a "physical science", together called the "physical sciences". However, the term "physical" creates an unintended, somewhat arbitrary distinction, since many branches of physical science also study biological phenomena (organic chemistry, for example). The four main branches of physical science are astronomy, physics, chemistry, and the Earth sciences, which include meteorology and geology *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/mtKLA');
+      }else if(cat=="Education"){
+        response = response.replace('No information available','Education is the transmission of knowledge, skills, and character traits. Its precise definition is disputed and there are disagreements about what the aims of education are and to what extent education is different from indoctrination by fostering critical thinking. These disagreements affect how to identify, measure, and improve forms of education. The term "education" can also refer to the mental states and qualities of educated people and the academic field studying educational phenomena. *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/qzDnZ');
+      }else if(cat=="Humanities & Liberal Arts"){
+        response = response.replace('No information available','Liberal arts education (from Latin liberalis "free" and ars "art or principled practice") is the traditional academic course in Western higher education. Liberal arts takes the term art in the sense of a learned skill rather than specifically the fine arts. Liberal arts education can refer to studies in a liberal arts degree course or to a university education more generally. Such a course of study contrasts with those that are principally vocational, professional, or technical, as well as religiously based courses. *via wikipedia');
+        response=response.replace('$$image.png$$', 'https://t.ly/LEX09');
+        }
+
       res.status(200).type('html').send(response);
-  
     }).catch((error)=>{
       res.status(404).type('txt').send(`404 Error: Major Category Not Found '${cat}' `);
     });
